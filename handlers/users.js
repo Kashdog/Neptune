dafd1bc26c1c9ee89b9121f32b040bb62d52a061
Aneesh Kashalikar <kashdog@Aneeshs-MacBook-Pro.local>
2018-09-09 20:16:32 -0700

    Fixing File Upload, Req.session.userId error

--- handlers/users.js
+++ handlers/users.js
@@ -8,6 +8,7 @@ const neo4j = require('neo4j-driver').v1;
 
       var driver = neo4j.driver('bolt://35.233.191.222:7687', neo4j.auth.basic("neo4j", 'bPCs40260$#'));
       var session = driver.session();
+
       
 exports.index = (req, res, next) => {
   try {
@@ -28,7 +29,7 @@ exports.index = (req, res, next) => {
       var currentUser = getCurrentUser(session);
       currentUser.then(function(result) {
         console.log(result.properties.username);
-        res.render("profile", {name: result.properties.name, title: result.properties.title, phonenumber: result.properties.phone, email: result.properties.email, location: result.properties.location, username: result.properties.username, bio: result.properties.bio, profilepic: result.properties.profilepic, resume: result.properties.resume});
+        res.render("profile", {name: result.properties.name, title: result.properties.title, phonenumber: result.properties.phonenumber, email: result.properties.email, location: result.properties.location, username: result.properties.username, bio: result.properties.bio, profilepic: result.properties.profilepic, resume: result.properties.resume});
      })
     }
   } catch(err) {
@@ -38,6 +39,7 @@ exports.index = (req, res, next) => {
 }
 
 exports.edit =  async (req, res, next) => {
+  console.log("this is the userID", req.session.userId);
   if(!req.session.userId || req.session.userId == ""){
     console.log("true");
     res.redirect("/auth/login");
@@ -61,18 +63,15 @@ exports.edit =  async (req, res, next) => {
 
 exports.update = async (req, res, next) => {
   try {
-    console.log(req.files);     
-                                
+    console.log(req.body);                            
     var register = function (session) {
-      return session.run("MATCH (n:User) WHERE n.id =~ {id} SET n.name = {name} SET n.username = {username} SET n.profilepic = {profilepic} SET n.location = {location} SET n.title = {title} SET n.phonenumber = {phonenumber} SET n.bio = {bio} SET n.linkedin = {linkedin} SET n.github = {github} SET n.resume = {resume} RETURN n",
+      return session.run("MATCH (n:User) WHERE n.id =~ {id} SET n.name = {name} SET n.profilepic = {profilepic} SET n.location = {location} SET n.title = {title} SET n.bio = {bio} SET n.linkedin = {linkedin} SET n.github = {github} SET n.resume = {resume} RETURN n",
       {
         id: req.session.userId,
-        username: req.body.username,
         name: req.body.name,
         profilepic: req.body.profilepic,
         location: req.body.location,
         title: req.body.title,
-        phonenumber: req.body.phonenumber,
         bio: req.body.bio,
         linkedin: req.body.linkedin,
         github: req.body.github,
